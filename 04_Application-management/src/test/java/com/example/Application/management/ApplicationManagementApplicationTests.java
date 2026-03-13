5f89e7bcc53350ba0b7ab787c48e2502eedd454f
Prapti Godshelwar <praptigodshelwar7@gmail.com>
2026-03-14 01:14:18 +0530

    Added SpringBatch project

---
+++ 04_Application-management/src/test/java/com/example/Application/management/ApplicationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Application.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
